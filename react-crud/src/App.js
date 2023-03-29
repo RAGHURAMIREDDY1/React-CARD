2876aca108e6a392146acbfe88e20db199c6a8cf
RAGHURAMIREDDY1 <raghuramireddy.maram20@gmail.com>
2023-03-29 11:04:09 +0530

    do update and delete

---
+++ react-crud/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Create from './components/create';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+function App() {
+    return ( < Router >
+        <
+        div className = "main" >
+        <
+        h2 className = "main-header" > React Crud operations < /h2>    <
+        div >
+        <
+        Create / >
+        <
+        /div>   <
+        /div>  <
+        /Router>
+    );
+}
+
+export default App;
